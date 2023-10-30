8d0076ad0770fa87011b52be2bc606756d8c74db
RubelHasan <rubelhasan306@gmail.com>
2023-10-30 14:15:02 +0000

    deleted one item from the db

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       console.log(req.query?.email);
     });
 
+    //delete products
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await usersProductCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
